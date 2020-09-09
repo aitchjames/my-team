2b105251bc79ecd104bc25bd4fb729e2b35118bf
Hayden <hayden@aitch.co>
2020-09-09 15:17:53 +1000

    progress update on about and contact pages

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ const buttonOpen = document.querySelector('.button-open')
 const buttonClose = document.querySelector('.button-close')
 const mobileWrapper = document.querySelector('.nav-mobile__wrapper')
 const mobileNav = document.querySelector('.nav-mobile')
+const buttonTestimonial = document.querySelectorAll('.about-testimonial__button')
+
+// Mobile Menu
 
 function openMobileNav() {
     mobileWrapper.classList.add('open')
@@ -19,4 +22,15 @@ buttonOpen.addEventListener('click', () => {
 
 buttonClose.addEventListener('click', () => {
     closeMobileNav()
+})
+
+// About Testimonial Cards
+
+buttonTestimonial.forEach(button => {
+    button.addEventListener('click', () => {
+        const content = button.previousElementSibling;
+
+        content.classList.toggle('active')
+        button.classList.toggle('active')
+    })
 })
